4833ff0324ded7edc096ba6eeb07dd62c726217f
Evan Belrose <belrose69140@gmail.com>
2025-05-26 14:14:54 +0200

    okayy

--- script.js
+++ script.js
@@ -39,13 +39,13 @@
 //             <h3>${product.title}</h3>
 //             <p>${product.price} €</p>
 //             `;
-            
+
 
 
 //             console.log(data);
 //         });
 //     })
-    
+
 // })
 
 
@@ -60,46 +60,60 @@
 //     console.log(`Données récupérées avec succès :`,data);
 //   })
 //   .catch(error => console.error('Erreur lors de la récupération des données :', error));
-  
+
 // };
 
 
 
 function fetchsneaks(lien) {
-    fetch(lien)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        let contenair = document.getElementById("product-container");
-        let principal = document.createElement("div");
-        principal.className = "principal";
-        principal.innerHTML = `
+  fetch(lien)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let contenair = document.getElementById("product-container");
+      let principal = document.createElement("div");
+      principal.className = "principal";
+      principal.innerHTML = `
                 <h2>${data.nomCommercial}</h2>
           <p>${data.phraseAccroche}</p>
           <button>${data.texteAppelAction}</button>
               `;
-        contenair.appendChild(principal);
-        data.produits.forEach((activiter) => {
-          
-            let card = document.createElement("div");
-          card.classList.add("carte");
-         
-          let name = document.createElement("h3");
-          name.textContent = activiter.nom;
-         
-          let description = document.createElement("p");
-          description.textContent = activiter.description;
-         
-          let image = document.createElement("img");
-          image.src = activiter["image-url"];
+      contenair.appendChild(principal);
+      data.produits.forEach((activiter) => {
 
-          card.appendChild(name);
-          card.appendChild(description);
-          card.appendChild(image);
-          contenair.appendChild(card);
-        });
+        let card = document.createElement("div");
+        card.classList.add("carte");
+
+        let name = document.createElement("h3");
+        name.textContent = activiter.nom;
+
+        let description = document.createElement("p");
+        description.textContent = activiter.description;
+
+        let image = document.createElement("img");
+        image.src = activiter["image-url"];
+
+        card.appendChild(name);
+        card.appendChild(description);
+        card.appendChild(image);
+        contenair.appendChild(card);
+
+
+        // // contenair.appendChild(temoignages);
+        // // data.temoignages.forEach((temoins)=>{
+
+        // //   let name = document.createElement("h2");
+        // //   name.textContent = temoins.prenom;
+
+        // //   let exp = document.createElement("h3");
+        // //   exp.textContent = temoins.typeExperience;
+
+        // //   let commentaires = document.createElement("p");
+        // //   commentaires.textContent = temoins.commentaire;
+        // })
       });
-  }
-  fetchsneaks(
-    "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json"
-  );
+    });
+}
+fetchsneaks(
+  "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json"
+);
